a8feeaac1877e874712102d346a2ee9471ddfaf5
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2024-04-20 03:04:05 +0900

    impl initial iced gui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use iced::widget::{button, column, text};
-use iced::{executor, window, Application, Command, Element, Event, Settings, Subscription, Theme};
+use iced::alignment::{Horizontal, Vertical};
+use iced::font::{Family, Weight};
+use iced::widget::{button, column, vertical_space, Button, Column, Container, Text};
+use iced::{
+    executor, window, Alignment, Application, Command, Element, Event, Font, Length, Padding,
+    Settings, Subscription, Theme,
+};
 
 use anyhow::Result;
 use windows::{
@@ -28,8 +33,7 @@ struct State {
 enum Message {
     IncrementPressed,
     WindowClosed,
-    EventOccurred(Event),
-    Exit,
+    ExternalEventOccured(Event),
 }
 
 impl Application for State {
@@ -62,43 +66,64 @@ impl Application for State {
                 // ウィンドウが閉じられたときの処理はここでは行わない
                 Command::none()
             }
-            Message::EventOccurred(event) => {
+            Message::ExternalEventOccured(event) => {
                 if let Event::Window(id, window::Event::CloseRequested) = event {
                     window::close(id)
                 } else {
                     Command::none()
                 }
             }
-            Message::Exit => window::close(window::Id::MAIN),
         }
     }
 
     fn view(&self) -> Element<Self::Message> {
-        "text".into()
-        /*column()
-        .push(
-            button(&mut self.increment_button, text("Increment"))
-                .on_press(Message::IncrementPressed),
+        Container::new(
+            column![
+                Text::new("日本語サンプル。上手く出ているだろうか").font(Font {
+                    family: Family::Name("IBM Plex Sans JP"),
+                    weight: Weight::Bold,
+                    ..Font::default()
+                }),
+                vertical_space(),
+                Button::new(
+                    Text::new("+")
+                        .width(Length::Fill)
+                        .height(Length::Fill)
+                        .horizontal_alignment(Horizontal::Center)
+                        .vertical_alignment(Vertical::Center)
+                        .size(40)
+                )
+                .on_press(Message::IncrementPressed)
+                .width(Length::Fill)
+                .padding(50),
+                Text::new(self.value.to_string())
+                    .width(Length::Fill)
+                    .horizontal_alignment(Horizontal::Center)
+                    .size(40),
+                vertical_space(),
+            ]
+            .align_items(Alignment::Center),
         )
-        .push(text(self.value.to_string()))
-        .into()*/
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .center_x()
+        .center_y()
+        .padding(50)
+        .into()
     }
 
     fn subscription(&self) -> Subscription<Self::Message> {
         // ウィンドウが閉じられたときに`Message::WindowClosed`を発行する
-        //window::close_subscription().map(|_| Message::WindowClosed)
-        event::listen().map(Message::EventOccurred)
+        /*event::listen().map(|event| match event {
+            window::Event::Closed => Some(Message::WindowClosed),
+            _ => None,
+        })*/
+        event::listen().map(Message::ExternalEventOccured)
     }
 }
 
 fn main() -> Result<()> {
-    match windows_tools::enable_visual_styles() {
-        Ok(_) => (),
-        Err(ref errors) => match errors {
-            windows_tools::EnableVStylesErrors::Win32(win32_error) => println!("{:?}", win32_error),
-            windows_tools::EnableVStylesErrors::Standard(err) => println!("{:?}", err),
-        },
-    };
+    windows_tools::enable_visual_styles()?;
     println!("Hello, World!");
     unsafe {
         MessageBoxW(
@@ -109,14 +134,34 @@ fn main() -> Result<()> {
         )
     };
     let mut last_value = 0;
-    loop {
-        let state = State {
-            value: last_value, // 前回の値を引き継ぐ
-            increment_button: button::State::new(),
-        };
-        let mut settings = Settings::default();
-        //settings.exit_on_close_request = false;
-        State::run(settings);
-    }
+    let state = State {
+        value: last_value, // 前回の値を引き継ぐ
+        increment_button: button::State::new(),
+    };
+    let mut settings = Settings::default();
+    settings.antialiasing = true;
+    settings.fonts = vec![
+        include_bytes!("../fonts/IBMPlexSansJP-Regular.ttf")
+            .as_slice()
+            .into(),
+        include_bytes!("../fonts/IBMPlexSansJP-Bold.ttf")
+            .as_slice()
+            .into(),
+    ];
+    settings.default_font = Font::with_name("IBM Plex Sans JP");
+    settings.window = window::Settings {
+        exit_on_close_request: false,
+        ..window::Settings::default()
+    };
+    //settings.window.exit_on_close_request = false;
+    State::run(settings);
+    unsafe {
+        MessageBoxW(
+            HWND::default(),
+            w!("Hello World!"),
+            w!("YYScreenTime_win-rs"),
+            MB_OK | MB_ICONINFORMATION,
+        )
+    };
     Ok(())
 }
