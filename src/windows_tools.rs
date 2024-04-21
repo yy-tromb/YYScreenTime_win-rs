942f253b9dd21e40215fe33a1ac4fd690daf1f1d
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2024-04-21 11:46:15 +0900

    change win32 error handle to macro_rules!

--- src/windows_tools.rs
+++ src/windows_tools.rs
@@ -19,39 +19,11 @@ use windows::{
     },
 };
 
-#[derive(Debug)]
-pub enum EnableVStylesErrors {
-    Win32(WIN32_ERROR),
-    Standard(std::io::Error),
-}
-
-impl std::fmt::Display for EnableVStylesErrors {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            EnableVStylesErrors::Win32(win32_error) => write!(f, "Win32 Error: {:?}", win32_error),
-            EnableVStylesErrors::Standard(err) => write!(f, "{:?}", err),
-        }
-    }
-}
-impl std::error::Error for EnableVStylesErrors {}
-
-#[derive(Debug)]
-struct MyWin32Err(u32);
-impl MyWin32Err {
-    fn new(win32_err: WIN32_ERROR) -> Self {
-        Self(win32_err.0)
-    }
-}
-impl std::fmt::Display for MyWin32Err {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Win32 Error: {:?}", &self.0)
-    }
-}
-impl std::error::Error for MyWin32Err {}
-
-#[inline]
-fn win32_to_anyhow(win32_error: WIN32_ERROR) -> anyhow::Error {
-    anyhow!("Win32 Error: {}", win32_error.0)
+#[macro_export]
+macro_rules! anyhow_win32 {
+    ($win32_error:expr) => {
+        anyhow!("Win32 Error: {}", $win32_error.0)
+    };
 }
 
 /*
@@ -77,7 +49,7 @@ pub fn enable_visual_styles() -> Result<()> {
     const ACTCTX_FLAG_SET_PROCESS_DEFAULT: u32 = 0x010;
     let mut tmp_dir = [0u16; MAX_PATH_USIZE + 1];
     if unsafe { GetTempPath2W(Some(&mut tmp_dir)) } == 0 {
-        return Err(win32_to_anyhow(unsafe { GetLastError() }));
+        return Err(anyhow_win32!(unsafe { GetLastError() }));
     }
     /*if tmp_dir.len() > MAX_PATH_USIZE - 14 {
         println!(
@@ -93,7 +65,7 @@ pub fn enable_visual_styles() -> Result<()> {
     let prefix = w!("nwg");
     let manifest_dir_raw = PCWSTR::from_raw(&tmp_dir as *const u16);
     if unsafe { GetTempFileNameW(manifest_dir_raw, prefix, 0, &mut tmp_path) } == 0 {
-        return Err(win32_to_anyhow(unsafe { GetLastError() }));
+        return Err(anyhow_win32!(unsafe { GetLastError() }));
     };
     let manifest_path_utf16 = PCWSTR::from_raw(&tmp_path as *const u16);
     let manifest_path_len = tmp_path
@@ -123,12 +95,12 @@ pub fn enable_visual_styles() -> Result<()> {
                     fs::remove_file(&manifest_path)?;
                     return Ok(());
                 } else {
-                    return Err(win32_to_anyhow(GetLastError()));
+                    return Err(anyhow_win32!(GetLastError()));
                 };
             }
         };
         if let Err(_err) = ActivateActCtx(handle, &mut activation_cookie) {
-            return Err(win32_to_anyhow(GetLastError()));
+            return Err(anyhow_win32!(GetLastError()));
         };
     };
     fs::remove_file(&manifest_path)?;
