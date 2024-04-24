637ce1d159d74c06a135a42397da61faddca18b2
yy-tromb <107352731+yy-tromb@users.noreply.github.com>
2024-04-24 12:20:15 +0900

    use crate ipc_channel

--- src/watcher.rs
+++ src/watcher.rs
@@ -1,5 +1,5 @@
-use tokio::net::windows::named_pipe;
-use tokio::process;
+//use tokio::net::windows::named_pipe;
+use tokio::process::Command;
 
-// use ipc_channel;
+use ipc_channel;
 // use remoc;
